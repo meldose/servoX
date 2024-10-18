e86ca54e550bef0daad60342d1f9779685b9f442
midhun.eldose <midhun.eldose>
2024-10-18 12:06:02 +0200

    added robot variable

--- servoX.py
+++ servoX.py
@@ -1,11 +1,12 @@
 # Servo control to track a part on the conveyor in the X-axis.
+from neurapy.robot import Robot
 conveyor_speed = 0.2  # Conveyor speed in meters per second.
 time_interval = 0.1   # Time interval in seconds for each update.
-
+r=Robot()
 # Function to track the object moving along X with the conveyor
 def servo_tracking_with_conveyor():
     while True:
-        current_position = get_current_position()  # Get current position of TCP
+        current_position = r.get_current_position()  # Get current position of TCP
         new_position = current_position
         new_position[0] += conveyor_speed * time_interval  # Update X position with conveyor speed
         servox(new_position, a=1.2, v=0.25)  # Execute servo movement with given acceleration and velocity
