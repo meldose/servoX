1c93cb6eab902b0b5cf62499d5044b79aa6d8b2f
eskinjoseph <eskin.joseph@neura-robotics.com>
2024-11-18 21:58:54 +0100

    added lines for exampleservo

--- exampleservo.py
+++ exampleservo.py
@@ -1,36 +1,39 @@
-from neurapy.robot import Robot # import robot module
-import time # import time module
-import copy # import copy moudule
-import signal # import signal module
-import sys # import sys module
-import numpy as np # import numpy module 
-import quaternion # impoort quaternion module
+from neurapy.robot import Robot
+import time
+import numpy as np
+import signal
+import sys
+
+def signal_handler(signum, frame):
+    print("Signal received, stopping robot")
+    r.stop()  # This should be the method to stop all robot movements
+    r.deactivate_servo_interface()
+    sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
-    # def signal_handler(signum, frame):
 
-r = Robot() # assigning the robot module to r variable
-
-#Switch to external servo mode
+r = Robot()
 r.activate_servo_interface('position')
 
-target_1 = [0.3, 0.25, 0.1] # added for testing the target position 
-target_2 = [0.25, 0.3, 0.2] # added second target position for testing 
+# Define detailed paths for each movement
+# Example: List of positions the robot should move through linearly
+path_1 = np.array([[0.3, 0.25, 0.1], [0.35, 0.25, 0.15], [0.3, 0.30, 0.1]])  # More points added
+path_2 = np.array([[0.25, 0.3, 0.2], [0.2, 0.35, 0.25], [0.25, 0.35, 0.2]])
 
-for target in [target_1, target_2]: # checking the values are there in target 1 and 2
-    print("Target:", target) # printing the target position
-    current_pose = copy.deepcopy(r.get_current_cartesian_pose()) # adding the current position to an variable
-    current_pose[:3] += target # adding the current position of the robot to the target position
-    velocity = [0.15]*6 # velocity is set to 0.15   
-    acceleration = [2.]*6 # acceleration is set to 2
-    error_code = r.movelinear_online(current_pose, velocity, acceleration)
-    time.sleep(10) # setting the time  for sleep as 10 seconds
-    target[0] -= target_1 # increasing the target position by 1
+paths = [path_1, path_2]
+for path in paths:
+    print("Moving through path:", path)
+    try:
+        # Assuming move_linear can take multiple points as a list of lists
+        error_code = r.move_linear(path.tolist(), velocity=0.15, acceleration=2.0)  # Adjust parameters as necessary
+        if error_code != 0:
+            print("Error encountered:", error_code)
+            break
+    except Exception as e:
+        print("An exception occurred:", str(e))
+        break
+    time.sleep(10)
 
-
-r.stop_movelinear_online() # stop the robot movement    
-print("Robot stopped") # robot stopped 
-time.sleep(2) # sleep for 2 seconds
-r.deactivate_servo_interface() # deactivating the servo interface
-r.stop() # stopped the robot
-
+r.deactivate_servo_interface()
+r.stop()
+print("Robot stopped")
