9da4615dcb578a6decfc2632ff7d66ed0be15a51
meldose <meldose@gmail.com>
2025-01-03 17:33:21 +0100

    move_linear.py

--- servoJ.py
+++ servoJ.py
@@ -1,45 +1,47 @@
 from neurapy.robot import Robot
 import time
 from ruckig import InputParameter, OutputParameter, Result, Ruckig
-
+ 
 r = Robot()
-
-def servo_j(self,*args,**kwargs):  
+ 
+def servo_j():  
     #Switch to external servo mode
     r.activate_servo_interface('position')
-
+ 
     dof = 6
-
+ 
     otg = Ruckig(dof, 0.001)  # DoFs, control cycle
     inp = InputParameter(dof)
     out = OutputParameter(dof)
-
+ 
     inp.current_position = r.get_current_joint_angles()
     inp.current_velocity = [0.]*dof
     inp.current_acceleration = [0.]*dof
-
-    inp.target_position = []
+ 
+    inp.target_position = [0.7171273227449193, -0.5909595974851617, -1.0818671607769956, 3.0130405925616444, 1.4414659178889175, -2.2522953507998986]
     inp.target_velocity = [0.]*dof
     inp.target_acceleration = [0.]*dof
-
+ 
     inp.max_velocity = [0.5]*dof
     inp.max_acceleration = [3]*dof
     inp.max_jerk = [10.]*dof
     res = Result.Working
-
+ 
+ 
+ 
     while res == Result.Working:
         '''
-        Error code is returned through Servo. 
+        Error code is returned through Servo.
         '''
         error_code = 0
         if(error_code < 3):
-
+ 
             res = otg.update(inp, out)
-
+ 
             position = out.new_position
-            velocity = out.new_velocity 
+            velocity = out.new_velocity
             acceleration = out.new_acceleration
-
+ 
             error_code = r.servo_j(position, velocity, acceleration)
             scaling_factor = r.get_servo_trajectory_scaling_factor()
             out.pass_to_input(inp)
@@ -47,8 +49,9 @@ def servo_j(self,*args,**kwargs):
         else:
             print("Servo in error, error code, ", error_code)
             break
+        
     r.deactivate_servo_interface()
-
+ 
     r.stop()
     
     
@@ -58,3 +61,6 @@ def servo_j(self,*args,**kwargs):
     
     command = Servo(self)
     add_additional_argument(command, *args, additional_args=(0), **kwargs)
+ 
+servo_j()
+ 
