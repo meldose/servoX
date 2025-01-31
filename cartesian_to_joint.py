1b7eb1c6aa4e3388654dbb2f1a786e6a4a038456
meldose <meldose@gmial.com>
2025-01-31 16:20:52 +0100

    cartesian_to_joint.py

--- cartesian_to_joint.py
+++ cartesian_to_joint.py
@@ -1,6 +1,6 @@
 from neurapy.robot import Robot
 r = Robot()
-target_angle = r.ik_fk("ik", target_pose =[-332,211,263, 3.0,0.16,-2.37], # object pose values in radians
-current_joint = [-0.456,-0.314,0.210,-3.02,-0.06,1.41]) # current joint angles in radians
+target_angle = r.ik_fk("ik", target_pose =[-0.456,-0.314,0.230,-3.02,-0.06,1.41], # object pose values in radians
+current_joint = [-0.332,0.211,0.263,3,0.16,-2.37]) # current joint angles in radians
 print(target_angle)
 
