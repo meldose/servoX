31458fc19dc8c8cb1041440a2e8be1afff73c042
meldose <meldose@gmial.com>
2025-02-13 14:41:33 +0100

    servoJ.py

--- cartesian_to_joint.py
+++ cartesian_to_joint.py
@@ -3,3 +3,4 @@ r = Robot()
 target_angle = r.ik_fk("ik", target_pose =[-0.456,-0.314,0.230,-3.02,-0.06,1.41],current_joint = [-0.332,0.211,0.263,3,0.16,-2.37]) # object pose values in radians 
  # current joint angles in radians
 print(target_angle)
+
--- servoJ.py
+++ servoJ.py
@@ -5,8 +5,17 @@ from ruckig import InputParameter, OutputParameter, Result, Ruckig # importing r
 r = Robot() #settig r as the variable for the Robot
 r.gripper("on") # setting gripper on
 
-def servo_j(): # defining function for servoJ
+def servo_j(message): # defining function for servoJ
     #Switch to external servo mode
+
+    x = message[0]
+    y = message[1]
+    z = message[2]
+    a = message[3]
+    b = message[4]
+    c = message[5]
+    d = message[6]
+
     r.activate_servo_interface('position') # activating the servo interface
     dof = 6 # setting the DOF as 6 
     otg = Ruckig(dof, 0.001)  # DoFs, control cycle
@@ -18,25 +27,25 @@ def servo_j(): # defining function for servoJ
     inp.current_velocity = [0.]*dof
     inp.current_acceleration = [0.]*dof
  
-    inp.target_position = [1.1625650370244778, -0.5774947959093657, -1.6300017754314295, 1.9807964651163987, 1.5676122261006906, 0.636066807616557] # target positon
- 
+    inp.target_position = [0.7523937541369765, -0.29802456012011835, -1.179023089002252, -0.13305920361488976, -1.6366873468026333, 0.8989122483477352] # target positon
+    #inp.target_position = [x,y,z,a,b,c,d]
     inp.max_velocity = [0.5]*dof # setting up the maximum velocity 
     inp.max_acceleration = [3]*dof # setting up the maximum acceleration
-
-    inp = InputParameter(dof) # setting the input parameter
-    out = OutputParameter(dof) # setting the ouput parameters 
-    inp.current_position = r.get_current_joint_angles() # getting the current joint angles
-    inp.current_velocity = [0.]*dof # setting the current velocity as zero
-    inp.current_acceleration = [0.]*dof # setting the current acceleration as zero
+   
+    # inp = InputParameter(dof) # setting the input parameter
+    # out = OutputParameter(dof) # setting the ouput parameters 
+    
+    # inp.current_position = r.get_current_joint_angles() # getting the current joint angles
+    # inp.current_velocity = [0.]*dof # setting the current velocity as zero
+    # inp.current_acceleration = [0.]*dof # setting the current acceleration as zero
  
-    inp.target_position = [1.1625650370244778, -0.5774947959093657, -1.6300017754314295, 1.9807964651163987, 1.5676122261006906, 0.636066807616557] # providing the target position
+    # inp.target_position = [0.7523937541369765, -0.29802456012011835, -1.179023089002252, -0.13305920361488976, -1.6366873468026333, 0.8989122483477352] # providing the target position
     inp.target_acceleration = [0.]*dof # setting the target acceleration as zero.
     r.gripper("on") # setting the gripper in On position.
- 
     inp.max_velocity = [0.5]*dof # defining the maximum velocity
     inp.max_acceleration = [3]*dof # defining the maximum acceleration
-
     inp.max_jerk = [10.]*dof
+    
     res = Result.Working
  
     while res == Result.Working:
@@ -49,14 +58,14 @@ def servo_j(): # defining function for servoJ
         acceleration = out.new_acceleration # setting the new acceleration 
  
         error_code = r.servo_j(position, velocity, acceleration) # passing the error code variable with having servo_j function having position, velocity and acceleration.
-        print(error_code) # checking if the error is there or not 
+        # print(error_code) # checking if the error is there or not 
         scaling_factor = r.get_servo_trajectory_scaling_factor() # getting the servo trajectory scaling factors.
         out.pass_to_input(inp)
         time.sleep(0.001) # setting the time sleep to 0.001 seconds
 
     r.deactivate_servo_interface() # deactivating the servo interface
  
-    r.stop() # stopped the robot
+    # r.stop() # stopped the robot
 
 servo_j() # calling the servo_j function
 r.gripper("off") # setting gripper off
