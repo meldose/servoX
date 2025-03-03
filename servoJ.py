d3490ed0d50012e31f3056bcf7d8a7c6b04068ef
meldose <meldose@gmial.com>
2025-03-03 15:34:37 +0100

    servoJ.py

--- servoJ.py
+++ servoJ.py
@@ -6,23 +6,7 @@ r = Robot() #settig r as the variable for the Robot
 r.gripper("on") # setting gripper on
 
 def servo_j(): # defining function for servoJ
-    # #Switch to external servo mode
-
-    # message = [x/1000 for x in message] # converting the values to mm
     
-    # x = message[0]  # setting the values
-    # y = message[1]  # setting the values
-    # z = message[2]  # setting the values
-    # a = message[3]  # setting the values
-    # b = message[4]  # setting the values
-    # c = message[5]  # setting the values
-    # d = message[6]  # setting the values
-    
-    # print(message) # printing the message
-    
-    # new_message = [x,y,z,d,a,b,c] # added new order for quaternion values
-    # print(new_message) # printing the new ordered message
-
     r.activate_servo_interface('position') # activating the servo interface
     dof = 6 # setting the DOF as 6 
     otg = Ruckig(dof, 0.001)  # DoFs, control cycle
@@ -34,7 +18,7 @@ def servo_j(): # defining function for servoJ
     inp.current_velocity = [0.]*dof
     inp.current_acceleration = [0.]*dof
  
-    inp.target_position = [0.561036550354565, -0.8075385726899302, -0.8665202488853906, -0.07588653308042255, -1.5022882144652068, 2.0441351475042864] # target positon
+    inp.target_position = [0.561036550354565, -0.8075385726899302, -0.8665202488853906, -0.07588653308042255, -1.5022882144652068, -2.0441351475042864] # target positon
     #inp.target_position = [x,y,z,a,b,c,d]
     inp.max_velocity = [1.0]*dof # setting up the maximum velocity 
     inp.max_acceleration = [5]*dof # setting up the maximum acceleration
