0a12f83cdb9bf9cf95816720a07dc7bf40f8d2a1
meldose <meldose@gmial.com>
2025-03-03 16:06:33 +0100

    imporved the acceleration and velocity

--- servoJ.py
+++ servoJ.py
@@ -24,7 +24,7 @@ def servo_j(): # defining function for servoJ
    
     inp.target_acceleration = [0.]*dof # setting the target acceleration as zero.
     r.gripper("on") # setting the gripper in On position.
-    inp.max_velocity = [4.0]*dof
+    inp.max_velocity = [8.0]*dof
     inp.max_acceleration = [6]*dof
     inp.max_jerk = [1.0]*dof
 
